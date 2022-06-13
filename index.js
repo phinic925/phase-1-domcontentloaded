df3b8af64c219aa8a631a0d00ee18be147512c08
phinic925 <phinicnyangate480@gmail.com>
2022-06-13 10:55:27 +0100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Your code goes here
+ 
+  document.addEventListener("DOMContentLoaded",function(){
+    let main = document.querySelector('#text');
+
+     main.textContent = "This is really cool!";
+
+ }
+ );
+ 
